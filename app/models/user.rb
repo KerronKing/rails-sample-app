dffb79716e7d3bde6d52cc3448daf64344007a3b
KerronKing <king_of_palmiste@hotmail.com>
2019-10-10 18:13:27 -0400

    Complete user and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
+  before_save: { self.email = email.downcase }
   validates :name,  
   presence: true, 
   length: { maximum: 24 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email, 
   presence: true, 
   length: { maximum: 255 }, 
-  format: { with: VALID_EMAIL_REGEX }
+  format: { with: VALID_EMAIL_REGEX },
   uniqueness: { case_sensitive: false }
+
+  
 end
