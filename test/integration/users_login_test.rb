dbcaf2fa59369502219d024f56f3b142707bf3a0
KerronKing <king_of_palmiste@hotmail.com>
2019-10-25 18:58:56 -0400

    Update user variables in sessions controller and login tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,4 +35,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_equal assigns(:user).remember_token, cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: "1")
+    log_in_as(@user, remember_me: "0")
+    assert_not_empty cookies[:remember_token]
+  end
 end
