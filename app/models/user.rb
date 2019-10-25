d08ba899797275389641961816a9c771bea6dc95
KerronKing <king_of_palmiste@hotmail.com>
2019-10-25 13:45:21 -0400

    Add forget methods to user model and sessions helper and update logout method in sessions helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,24 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
-  validates :name,  
-  presence: true, 
-  length: { maximum: 24 }
+  validates :name,
+            presence: true,
+            length: { maximum: 24 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, 
-  presence: true, 
-  length: { maximum: 255 }, 
-  format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+      BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -33,5 +33,9 @@ class User < ApplicationRecord
 
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
