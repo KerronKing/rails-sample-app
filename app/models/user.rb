2cae57f61efbabc61f8a4a938824d851ca2319d8
KerronKing <king_of_palmiste@hotmail.com>
2019-10-31 13:55:05 -0400

    Setup user mailer email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
   before_create :create_activation_digest
-  before_save { self.email = email.downcase }
   validates :name,
             presence: true,
             length: { maximum: 24 }
@@ -41,6 +40,8 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
 
   def downcase_email
     self.email = email.downcase
